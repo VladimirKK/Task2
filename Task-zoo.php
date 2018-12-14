c4cf9def489fafa8c8f0432b161c95622f85106f
VladimirKK <42947905+VladimirKK@users.noreply.github.com>
2018-12-14 20:47:18 +0300

    Работа 2

---
+++ Task-zoo.php
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+<?php
+$array = ["Ursus arctos", "Sciurus", "Dipodidae", "Machairodontinae", "Panthera tigris", "Leporidae", "Alces alces", "Talpidae", "Cervidae", "Castor fiber", "Canis lupus"];
+
+$d = array_fill_keys($array, 'evrazia');
+print_r($d);
+?>
+
+
+<?php
+ $k = str_word_count($array);
+
+$array = ["Ursus arctos", "Sciurus", "Dipodidae", "Machairodontinae", "Panthera tigris", "Leporidae", "Alces alces", "Talpidae", "Cervidae", "Castor fiber", "Canis lupus"];
+$text_nonspace=str_replace(array(" "), '', $array);
+echo "<br>" . "<br>";
+
+if
+
+
+//foreach ($array as $key => $value) {
+  //echo "$key: $value\n";
+//}
+
+// $input = [1 => "a", "b", "c", "d"];
+// print_r(array_slice($input, 0, 4));
+// echo "</br>";
+
+?>
